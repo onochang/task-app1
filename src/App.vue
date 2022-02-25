e746c1981516be4d8f04920ff37da10155da1f45
yasumasa ono <yasumasa.ono0725@gmail.com>
2022-02-25 15:30:20 +0900

    task-app day2

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <fa :icon="['fas','leaf']"/>
-  <fa :icon="['far','clock']" />
-  <router-view/>
+  <header class="header">
+    <fa class="fa-bars" :icon="['fas','bars']" />
+    <fa class="fa-sign-out" :icon="['fas','arrow-right-from-bracket']" />
+  </header>
+  <SideNav />
+  <main>
+    <router-view/>
+  </main>
 </template>
 
 <script>
 import 'normalize.css'
+import SideNav from './components/SideNav.vue'
 
 export default {
   name: "App",
+  components: {
+    SideNav,
+  }
 }
 
 </script>
@@ -22,8 +27,36 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  // text-align: center;
+  // color: #2c3e50;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+h1,h2,p{
+  margin: 0;
+}
+
+body {
+  background-color: #8A47CC;
+}
+
+.header {
+  background-color: #562D80;
+  height: 56px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  box-shadow: 0px 3px 6px rgba($color: #000000, $alpha: 0.16);
+}
+
+.fa-bars,.fa-sign-out {
+  color: #FFFFFF;
+  font-size: 20px;
 }
 
 nav {
